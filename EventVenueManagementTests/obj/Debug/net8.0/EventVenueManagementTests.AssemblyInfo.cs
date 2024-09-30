e782c3b05730bf42a4d67c95ba4884837ec0e2f7
JavierStark <javiertorralbocortes@gmail.com>
2024-09-30 11:19:58 +0200

    project: api

--- EventVenueManagementTests/obj/Debug/net8.0/EventVenueManagementTests.AssemblyInfo.cs
+++ EventVenueManagementTests/obj/Debug/net8.0/EventVenueManagementTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventVenueManagementTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f332ea015e9fd64228346ad0290fe1044d7ef54a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51939a3eface15b3cae3c66c0e508e68b686299")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventVenueManagementTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventVenueManagementTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
