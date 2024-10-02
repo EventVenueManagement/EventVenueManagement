16476392fd9199222458f34e8550116d65387c64
JavierStark <javiertorralbocortes@gmail.com>
2024-10-02 13:38:37 +0200

    test: event brief

--- EventVenueManagementCore/Event.cs
+++ EventVenueManagementCore/Event.cs
@@ -19,4 +19,5 @@ public class Event
 
 public enum EventType
 {
+    Concert
 }
--- EventVenueManagementTests/EventTests.cs
+++ EventVenueManagementTests/EventTests.cs
@@ -1,15 +1,32 @@
+using EventVenueManagementCore;
+
 namespace EventVenueManagementTests;
 
 public class EventTests
 {
-    [SetUp]
-    public void Setup()
+    
+    public static Event GenerateRandomEvent()
     {
+        return new Event
+        {
+            Name = Guid.NewGuid().ToString(),
+            RecommendedAge = 0,
+            ImageUrl = "",
+            Summary = "",
+            Price = 0,
+            Type = EventType.Concert
+        };
     }
-
     [Test]
-    public void Test1()
+    public void GetBrief()
     {
-        
+        var sut = GenerateRandomEvent();
+
+        sut.GetBrief().Should().BeEquivalentTo(new Event.EventBrief(
+            sut.Name,
+            sut.Summary,
+            sut.ImageUrl,
+            sut.RecommendedAge
+        ));
     }
 }
