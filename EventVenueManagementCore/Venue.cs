bf36b4ed99aff3343287b0a4585b16cc2b2aa933
JavierStark <javiertorralbocortes@gmail.com>
2024-10-02 13:39:49 +0200

    feat: add event to venue

---
+++ EventVenueManagementCore/Venue.cs
@@ -0,0 +1,15 @@
+﻿namespace EventVenueManagementCore;
+
+public class Venue
+{
+    private readonly List<Event> events = new();
+    public virtual IEnumerable<Event> GetEvents()
+    {
+        return events;
+    }
+
+    public void AddEvent(Event newEvent)
+    {
+        events.Add(newEvent);
+    }
+}
---
+++ EventVenueManagementTests/VenueTests.cs
@@ -0,0 +1,26 @@
+﻿using EventVenueManagementCore;
+
+namespace EventVenueManagementTests;
+
+public class VenueTests
+{
+    [Test]
+    public void NewVenueWithoutEvents()
+    {
+        var sut = new Venue();
+
+        sut.GetEvents().Should().BeEmpty();
+    }
+    
+    [Test]
+    public void VenueWithOneEvent()
+    {
+        var sut = new Venue();
+        var randomEvent = EventTests.GenerateRandomEvent();
+        sut.AddEvent(randomEvent);
+        
+        sut.GetEvents().Should().BeEquivalentTo(new List<Event> { randomEvent });
+    }
+
+    
+}
