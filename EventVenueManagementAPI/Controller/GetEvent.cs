52e1c4272e71ebb56095e96d82d60e5ff36b4fef
JavierStark <javiertorralbocortes@gmail.com>
2024-10-04 13:50:09 +0200

    feat: improve GetEvent response when missing event

--- EventVenueManagementAPI/Controller/GetEvent.cs
+++ EventVenueManagementAPI/Controller/GetEvent.cs
@@ -4,10 +4,11 @@ using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace EventVenueManagementAPI.Controller;
 
-public class GetEvent(Venue model) : GetController<string, Ok<Event>>
+public class GetEvent(Venue model) : GetController<string, Results<NotFound, Ok<Event>>>
 {
-    public Ok<Event> Execute(string name)
+    public Results<NotFound, Ok<Event>> Execute(string name)
     {
-        return TypedResults.Ok(model.GetEvent(name));
+        var @event = model.GetEvent(name);
+        return @event == null ? TypedResults.NotFound() : TypedResults.Ok(@event);
     }
 }
