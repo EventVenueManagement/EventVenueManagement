dfb79e3502294ec8d3edaca2fbe14ba3cc76137c
JavierStark <javiertorralbocortes@gmail.com>
2024-11-18 09:17:26 +0100

    fix: missing packages

---
+++ EventVenueManagementAPI/EventVenueDB.cs
@@ -0,0 +1,10 @@
+﻿using EventVenueManagementCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventVenueManagementAPI;
+
+public class EventVenueDB : DbContext
+{
+    public DbSet<Venue> Venues { get; set; }
+    public DbSet<Event> Events { get; set; }
+}
