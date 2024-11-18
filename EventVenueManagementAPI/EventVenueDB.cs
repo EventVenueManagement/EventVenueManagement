b528cae5aa84de1b2fadbb2bc608d81839803196
JavierStark <javiertorralbocortes@gmail.com>
2024-11-18 09:25:28 +0100

    fix: missing constructor in EventVenueDB

--- EventVenueManagementAPI/EventVenueDB.cs
+++ EventVenueManagementAPI/EventVenueDB.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EventVenueManagementAPI;
 
-public class EventVenueDB : DbContext
+public class EventVenueDB(DbContextOptions<EventVenueDB> options) : DbContext(options)
 {
     public DbSet<Venue> Venues { get; set; }
     public DbSet<Event> Events { get; set; }
